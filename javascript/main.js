8d5463248cb9f8d7a67355f8ec94e75e6d5fd570
Andy <amillion3@gmail.com>
2018-03-09 21:37:23 -0600

    code kind of works. it will translate only spanish and only the initial value entered into the textarea.

--- javascript/main.js
+++ javascript/main.js
@@ -18,17 +18,44 @@ function addButtonEvtListener() {
   document.getElementById("buttons").addEventListener("click", function(e) {
     if (e.target && e.target.nodeName == "button");
       var funcGatherUserInput = gatherUserInput();
+      var funcMatchWords = matchWords(funcGatherUserInput, e.target.id);
       //  e.target.id  use to identify which language
       //call translate function
   });
-
 }
-
-addButtonEvtListener();
 
 function gatherUserInput() {
   var input = document.getElementById("translate-me").value;  //collects input from textarea element
-  input = input.replace(/[^A-Za-z\s]/g, "").toLowerCase();  // keeps only letters and spaces
+  // input = input.replace(/[^A-Za-z\s]/g, "").toLowerCase();  // keeps only letters and spaces
   var inputArr = input.split(" ");  //splits and stores in an array
   return inputArr;
 }
+
+function matchWords(inputArray, language) {
+  //how to make language work dynamically select correct library???
+  //i don't like this solution, but i should make it a function if i keep it
+  if (language == "esperanto") {
+    var langDictionary = esperanto;
+  } else if (language == "german") {
+    var langDictionary = german;
+  } else {
+    var langDictionary = spanish;
+  }
+  //boo
+
+  var matchedArrays = [];
+  //should use forEach() instead
+  for (var key in inputArray) {  // for...in to loop through the user input array
+    for (var keyLang in langDictionary) {  // for...in to loop through the dictionary object
+      if (inputArray[key] === langDictionary[keyLang]) {
+        // console.log("match found, input: ", inputArray[key]);
+        // console.log("match found, dict: ", langDictionary[keyLang]);
+        matchedArrays.push(keyLang);
+        // console.log(keyLang);
+        document.getElementById("output-div").innerHTML += " - " + keyLang;
+      }
+    } // for in   Object.keys(obj)
+  }  //for in
+}
+
+addButtonEvtListener();
