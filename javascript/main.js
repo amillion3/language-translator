c09139ff5093d1715d1e5a10e11448f4e485debf
Andy <amillion3@gmail.com>
2018-03-09 19:34:04 -0600

    created variables and constructor function to create dictionary objects. also updated index.html so textarea had an id.

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,15 @@
-console.log("hello");
+var userInput = "";
+var userInputArr = [];
+
+function LanguageDictionary(merry, christmas, and, happy, newnew, year) {
+  this.merry = merry;
+  this.christmas = christmas;
+  this.and = and;
+  this.happy = happy;
+  this.newnew = newnew;
+  this.year = year;
+}
+
+var esperanto = new LanguageDictionary("feliĉan","kristnaskon","kaj","feliĉa","nova","jaro");
+var german = new LanguageDictionary("fröhliche","weihnachten","und","glücklich","neu","jahr");
+var spanish = new LanguageDictionary("feliz","navidad","y","feliz","nuevo","año");
